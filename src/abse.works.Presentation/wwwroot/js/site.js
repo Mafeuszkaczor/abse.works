d2c4f5ba2ee249bc3b6a2e8a0cb1a560164fe07f
Mateusz Kaczor <mafeuszkaczor@gmail.com>
2025-05-28 23:32:18 +0200

    fix configuration builder

--- src/abse.works.Presentation/wwwroot/js/site.js
+++ src/abse.works.Presentation/wwwroot/js/site.js
@@ -1,24 +1,4 @@
-﻿const notyf = new Notyf({
-    duration: 1000,
-    position: {
-        x: 'right',
-        y: 'top',
-    },
-    types: [
-        {
-            type: 'warning',
-            background: 'orange',
-            icon: {
-                className: 'material-icons',
-                tagName: 'i',
-                text: 'warning'
-            }
-        },
-        {
-            type: 'error',
-            background: 'indianred',
-            duration: 2000,
-            dismissible: true
-        }
-    ]
-});
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
